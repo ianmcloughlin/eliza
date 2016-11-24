b899d4726989ac4910eda9a341e7c9c80fadc003
Ian McLoughlin <Ian.McLoughlin@gmit.ie>
2016-11-24 15:53:40 +0000

    Restarted git.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Eliza in Go
+
+This is an example of the Eliza AI chat bot coded in [Go](http://golang.org) for the module **Emerging Technologies**.
+The module is taught to fourth year undergraduate students at [GMIT](http://www.gmit.ie).
+The lecturer is [Ian McLoughlin](https://ianmcloughlin.github.io).
+
+The code here is adapted from [smallsurething.com](https://www.smallsurething.com/implementing-the-famous-eliza-chatbot-in-python/).
+
+#### Compilation
+[Go](https://golang.org) must be installed to run the code.
+It can then be run like in this example:
+```bash
+$ go build eliza.go
+$ ./eliza
+```
---
+++ eliza.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type substitution struct {
+	original   *regexp.Regexp
+	substitute string
+}
+type response struct {
+	question *regexp.Regexp
+	answers  []string
+}
+
+type Eliza struct {
+	responses     []response
+	substitutions []substitution
+}
+
+func (me *Eliza) readsubstitutions(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(bufio.NewReader(file))
+	scanner.Split(bufio.ScanLines)
+
+	for readoriginal := false; scanner.Scan(); {
+		s := scanner.Text()
+
+		switch {
+		case strings.HasPrefix(s, "#") || len(s) == 0:
+			// Do nothing
+		case readoriginal == false:
+			me.substitutions = append(me.substitutions, substitution{original: regexp.MustCompile(s)})
+			readoriginal = true
+		default:
+			me.substitutions[len(me.substitutions)-1].substitute = s
+			readoriginal = false
+		}
+	}
+}
+
+func (me *Eliza) readresponses(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(bufio.NewReader(file))
+	scanner.Split(bufio.ScanLines)
+
+	for newsection := true; scanner.Scan(); {
+		s := scanner.Text()
+
+		switch {
+		case strings.HasPrefix(s, "#"):
+			// Do nothing
+		case len(s) == 0:
+			newsection = true
+		case newsection == true:
+			me.responses = append(me.responses, response{question: regexp.MustCompile(s)})
+			newsection = false
+		default:
+			me.responses[len(me.responses)-1].answers = append(me.responses[len(me.responses)-1].answers, s)
+		}
+	}
+}
+
+func (me *Eliza) analyse(userinput string) string {
+	// Remove any whitespace at the start and end of the user input.
+	// userinput = strings.TrimSpace(userinput)
+
+	// Loop through the responses, looking for a match for the user input.
+	for _, response := range me.responses {
+		if matches := response.question.FindStringSubmatch(userinput); matches != nil {
+
+			// Select a random answer.
+			answer := response.answers[rand.Intn(len(response.answers))]
+
+			// Fill the answer with the captured groups from the matches.
+			for i, match := range matches[1:] {
+				// Reflect the pronouns in the captured group.
+				for _, sub := range me.substitutions {
+					match = sub.original.ReplaceAllString(match, sub.substitute)
+				}
+				// Replace $1 with the first reflected captured group, $2 with the second, etc.
+				answer = strings.Replace(answer, "$"+strconv.Itoa(i+1), match, -1)
+			}
+
+			// Send the filled answer back.
+			return answer
+		}
+	}
+
+	return "I don't know what to say."
+}
+
+func main() {
+	eliza := Eliza{}
+
+	eliza.readsubstitutions("data/substitutions.txt")
+	eliza.readresponses("data/responses.txt")
+
+	fmt.Println("Hello, I'm Eliza. How are you feeling today?")
+
+	for reader := bufio.NewReader(os.Stdin); ; {
+		fmt.Print("> ")
+		userinput, _ := reader.ReadString('\n')
+		userinput = strings.Trim(userinput, "\r\n")
+
+		fmt.Println(eliza.analyse(userinput))
+		
+		if strings.Compare(strings.ToLower(strings.TrimSpace(userinput)), "quit") == 0 {
+			break
+		}
+	}
+}
