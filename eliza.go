0486aa5919da0ea5dbcca9469fa517dc58abf21c
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2017-08-01 17:16:09 +0100

    added some comments.

--- eliza.go
+++ eliza.go
@@ -11,45 +11,90 @@ import (
 	"strings"
 )
 
+// A data structure representing a term that should be replaced in a string.
+// original is a regular expression to be matched, and substitute is a string to replace the match with.
+// An example use is to replace the word you with the word me.
 type substitution struct {
 	original   *regexp.Regexp
 	substitute string
 }
+
+// A data structure representing a user input and a list of responses to it that Eliza can give.
+// question is a regular expression representing the user input.
+// answers is an array of strings, any of which is a reasonable response to question.
+// question can capture groups of characters, and elements of answers can use them.
+// $1 is the first match, $2 the second, etc.
 type response struct {
 	question *regexp.Regexp
 	answers  []string
 }
 
+// A data structure representing Eliza.
+// responses is an array containing elements of type response, as above.
+// Likewise, substitutions is an array containing elements of type substitution.
+// The order of the elements in both arrays matters - the responses and substitutions are matched in order.
 type Eliza struct {
 	responses     []response
 	substitutions []substitution
 }
 
+// Method to read in a text file containing substitutions data.
+// It takes a single argument, a string, which is the path to the substitutions data file.
+// The file should have the following format:
+//   All lines that begin with a hash symbol are comments, and are ignored.
+//   Each section of the file should begin with at least one blank line.
+//   The next line should be a regular expression for what to substitute.
+//   The next line should be the new text for the substitution.
+//   After that, there should be at least one blank.
+// An example substitutions file is given in data/substitutions.txt.
 func (me *Eliza) readsubstitutions(path string) {
+
+	// Open the file, logging a fatal error if it fails.
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Set up a line-by-line scanner for the file.
 	scanner := bufio.NewScanner(bufio.NewReader(file))
 	scanner.Split(bufio.ScanLines)
 
+	// Read through the file line by line.
+	// readoriginal is false if we have not yet read the regular expression to match.
+	// It is true if we have read the regular expression, and are now looking for the substitution string.
 	for readoriginal := false; scanner.Scan(); {
+		// Get the text on the current line.
 		s := scanner.Text()
 
+		// Decide what to do with the line.
 		switch {
-		case strings.HasPrefix(s, "#") || len(s) == 0:
-			// Do nothing
-		case readoriginal == false:
-			me.substitutions = append(me.substitutions, substitution{original: regexp.MustCompile(s)})
-			readoriginal = true
-		default:
-			me.substitutions[len(me.substitutions)-1].substitute = s
-			readoriginal = false
+			// If the line is blank or starts with a # character then skip it.
+			case strings.HasPrefix(s, "#") || len(s) == 0:
+				// Do nothing
+
+			// If we haven't read the original, then append an element to the substitutions array.
+			// The regualr expression is compiled, and the substitution is left blank for now.
+			case readoriginal == false:
+				me.substitutions = append(me.substitutions, substitution{original: regexp.MustCompile(s)})
+				readoriginal = true
+			// Otherwise read the substitution and assign it to the last element of the substitutions array.
+			default:
+				me.substitutions[len(me.substitutions)-1].substitute = s
+				readoriginal = false
 		}
 	}
 }
 
+// Function to read in a text file containing responses data.
+// The file should have the following format:
+// All lines that begin with a hash symbol are comments, and are ignored.
+// This file should have the following format:
+//   Each section of the file should begin with at least one blank line.
+//   The next line should be a regular expression, matching a user input.
+//   Each subsequent line, until a blank line, should contain a response to
+//   the usr input. One of these will be chosen at random upon user input.
+//   After the responses, there should be at least one blank.
+// An example responses file is given in data/responses.txt.
 func (me *Eliza) readresponses(path string) {
 	file, err := os.Open(path)
 	if err != nil {
